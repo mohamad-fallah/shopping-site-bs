c3dc8c6fe1aade10edac8491e9141ae47f64979e
mohamad-fallah <mfallahfaal@gmail.com>
2023-07-22 23:17:00 +0330

    complet special 1

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 var now = new Date().getTime();
-var countDownDuration = 1 * 1 * 10 * 0 * 1000; // 365 روز به میلی‌ثانیه
+var countDownDuration = 10 * 20 * 10 * 60 * 1000; // 365 روز به میلی‌ثانیه
 var countDownDate = now + countDownDuration;
 
 var x = setInterval(function() {
@@ -11,10 +11,6 @@ var x = setInterval(function() {
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    document.getElementById("demo").innerHTML = '<span id="day">' + days + '</span>' +
-                                                 '<span id="hours">' + hours + '</span>' +
-                                                 '<span id="minutes">' + minutes + '</span>' +
-                                                 '<span id="seconds">' + seconds + '</span>';
     var z = document.getElementsByClassName("demos");
     for (var i = 0; i < z.length; i++) {
         z[i].innerHTML = 
