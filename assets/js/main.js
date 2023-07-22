6ee48aaf8e975253290b2d083ba8151037588e8b
mohamad-fallah <mfallahfaal@gmail.com>
2023-07-22 22:24:11 +0330

    coplet expire page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 var now = new Date().getTime();
-var countDownDuration = 14 * 24 * 60 * 60 * 1000; // 365 روز به میلی‌ثانیه
+var countDownDuration = 1 * 1 * 10 * 0 * 1000; // 365 روز به میلی‌ثانیه
 var countDownDate = now + countDownDuration;
 
 var x = setInterval(function() {
@@ -15,9 +15,39 @@ var x = setInterval(function() {
                                                  '<span id="hours">' + hours + '</span>' +
                                                  '<span id="minutes">' + minutes + '</span>' +
                                                  '<span id="seconds">' + seconds + '</span>';
-
+    var z = document.getElementsByClassName("demos");
+    for (var i = 0; i < z.length; i++) {
+        z[i].innerHTML = 
+        '<span id="day">' +
+        days + 
+        "</span>" +
+        '<span id="hours">' +
+        hours + 
+        "</span>" +
+        '<span id="minutes">' +
+        minutes + 
+        "</span>" +
+        '<span id="seconds">' +
+        seconds + 
+        "</span>";
+    }                                                                                       
     if (distance < 0) {
         clearInterval(x);
-        document.getElementById("demo").innerHTML = "زمان به پایان رسید!";
+        var y = document.getElementsByClassName("demos");
+        for (var i = 0; i < z.length; i++) {
+            y[i].innerHTML = "";
+        }
+        var g = document.getElementsByClassName("after-expire");
+        for (var i = 0; i < z.length; i++){
+            g[i].classList.add("offer-expire-text");
+        } 
+        var d = document.getElementsByClassName("offer-expire-text-inner");
+        for (var i = 0; i < z.length; i++){
+            d[i].innerHTML = "پیشنهاد ویژه این محصول به پایان رسید!";
+        } 
+        var t = document.getElementsByClassName("offer-blur");
+        for (var i = 0; i < z.length; i++){
+            t[i].style.filter = "blur(2px)";
+        } 
     }
 }, 1000);
