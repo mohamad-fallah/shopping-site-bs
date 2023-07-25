4325526fd15217bb5e6b850500b9ea2942d4b7a5
mohamad-fallah <mfallahfaal@gmail.com>
2023-07-25 16:37:21 +0330

    finish the 99% shopping site

--- assets/js/main.js
+++ assets/js/main.js
@@ -66,3 +66,9 @@ $('.owl-carousel').owlCarousel({
         }
     }
 })
+
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+const popover = new bootstrap.Popover('.popover-dismiss', {
+    trigger: 'hover'
+  })
